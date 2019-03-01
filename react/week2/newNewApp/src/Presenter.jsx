9ef88978404caa622504f9dcc0aed06b94152cd2
Yana Khuchuieva <yanakhuchuieva@gmail.com>
2019-03-01 11:18:39 +0100

    Add css for buttons

--- react/week2/newNewApp/src/Presenter.jsx
+++ react/week2/newNewApp/src/Presenter.jsx
@@ -7,25 +7,31 @@ const Presenter = (props) => {
         hour: 'numeric', minute: 'numeric', second: 'numeric'
     }
 
-    return <div>
-        <form>
-            <label>Enter description:</label><br />
+    return <div id="mainContainer">
+        <form id="form">
+            <label className="label">Enter description:</label><br />
             <input placeholder="title" className='input'
                 onChange={props.onChange}></input><br />
             <button onClick={props.addTodo} className="button">add</button>
         </form>
-        <h1 id='title'>My ToDo List</h1>
-        <ul id='container'>
-            {todo.map((item, idx) =>
-                <div id={`todoContainer${idx}`} className='todos'>
-                    <li className="firstLi" style={item.done ? {  color:"green" } : { textDecoration: "line-through", color: "red" }} key={idx}>ToDo: {item.title}</li>
-                    <li key={idx}>Date: {item.date.toLocaleDateString('en-US', options)}</li>
-                    <div className="ActionBtn">
-                        <button onClick={() => { props.onClick2(idx) }} className="editBtn">done</button>
-                        <button key={idx} className="deleteBtn" onClick={() => { props.onClick(idx) }}>delete</button>
-                    </div>
-                </div>)}
-        </ul>
+
+        <div>
+            <h1 id='title'>My ToDo List</h1>
+            <ul id='container'>
+                {todo.map((item, idx) =>
+                    <div id={`todoContainer${idx}`} className='todos'>
+                        <li className="firstLi" style={item.done ? { color: "green" } : { textDecoration: "line-through", color: "red" }} key={idx}>ToDo: {item.title}</li>
+                        <li key={idx}
+                            style={item.done ? { color: "black" } : { textDecoration: "line-through" }}>
+                            Date: {item.date.toLocaleDateString('en-US', options)}
+                        </li>
+                        <div className="ActionBtn">
+                            <button onClick={() => { props.onClick2(idx) }} id="editBtn">done</button>
+                            <button key={idx} id="deleteBtn" onClick={() => { props.onClick(idx) }}>delete</button>
+                        </div>
+                    </div>)}
+            </ul>
+        </div>
     </div>
 }
 
